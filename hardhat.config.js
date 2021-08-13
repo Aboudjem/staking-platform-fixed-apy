313296177b980d1d1214512db782ed177696db61
aboudjem <boudjemaa.adam@gmail.com>
2021-08-13 22:39:42 +0400

    :sparkles: add hardhat-gas-reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
+require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,6 +19,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  gasReporter: {
+    enabled: true
+  },
   networks: {
     hardhat: {
       hardfork: "berlin"
