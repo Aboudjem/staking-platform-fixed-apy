fcf8d0da56abce146a7e585554727d62fca8d61e
aboudjem <boudjemaa.adam@gmail.com>
2021-09-11 13:41:27 +0400

    :sparkles: add Token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Treeb", "TREEB") {
+    constructor(uint initialSupply) ERC20("Treeb", "TREEB") {
         _mint(msg.sender, initialSupply);
     }
 }
