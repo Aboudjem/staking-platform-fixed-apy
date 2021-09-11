e373adefb69b43dedf97cd2fdf674867b6971ab5
aboudjem <boudjemaa.adam@gmail.com>
2021-09-11 13:41:04 +0400

    :sparkles: add Tester contract for internal functions

---
+++ contracts/Tester.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "./StakingPlatform.sol";
+
+contract Tester is StakingPlatform {
+    constructor(
+        address _token,
+        uint _duration,
+        uint _rewards,
+        uint8 _fixedAPY
+    ) StakingPlatform(_token, _duration, _rewards, _fixedAPY) {}
+
+    function setPrecision(uint _precision) public {
+        precision = 10**_precision;
+    }
+
+    function calculatedReward() external view returns (uint) {
+        return _calculatedReward();
+    }
+
+    function percentageTimeRemaining() external view returns (uint) {
+        return _percentageTimeRemaining();
+    }
+}
