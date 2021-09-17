e6f916b2a9cec5350d3b5ec3fd2deb59d52ecfac
Aboudjem <boudjemaa.adam@gmail.com>
2021-09-17 15:01:50 +0400

    :bulb: add natspec comments

--- contracts/staking/IStakingPlatform.sol
+++ contracts/staking/IStakingPlatform.sol
@@ -1,39 +1,76 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+/// @author RetreebInc
+/// @title Interface Staking Platform with fixed APY and lockup
 interface IStakingPlatform {
+    /**
+     * @notice function that start the staking
+     * @dev set `startPeriod` to the current current `block.timestamp`
+     * as well as the `endPeriod` which is `startPeriod` + `stakingDuration`
+     */
     function startStaking() external;
 
+    /**
+     * @notice function that allows a user to deposit tokens
+     * @dev user must first approve the amount to deposit before calling this function,
+     * cannot exceed the `maxAmountStaked`
+     * @param amount, the amount to be deposited
+     */
     function deposit(uint amount) external;
 
+    /**
+     * @notice function that allows a user to withdraw its initial deposit
+     * @dev must be called only when `block.timestamp` >= `endPeriod`
+     */
     function withdraw() external;
 
+    /**
+     * @notice function that returns the amount of total Staked tokens
+     * for a specific user
+     * @return uint amount of the total deposited Tokens by the caller
+     */
     function amountStaked() external view returns (uint);
 
+    /**
+     * @notice function that returns the amount of total Staked tokens
+     * on the smart contract
+     * @return uint amount of the total deposited Tokens
+     */
     function totalDeposited() external view returns (uint);
 
+    /**
+     * @notice function that returns the amount of pending rewards
+     * that can be claimed by the user
+     * @param stakeHolder, address of the user to be checked
+     * @return uint amount of claimable tokens by the caller
+     */
     function rewardOf(address _stakeHolder) external view returns (uint);
 
+    /**
+     * @notice function that claims pending rewards
+     * @dev transfer the pending rewards to the user address
+     */
     function claimRewards() external;
 
     /**
-     * @dev Emitted when `value` tokens are deposited into
+     * @dev Emitted when `amount` tokens are deposited into
      * staking platform
      */
     event Deposit(address indexed owner, uint amount);
 
     /**
-     * @dev Emitted when user withdraw deposited amount
+     * @dev Emitted when user withdraw deposited `amount`
      */
     event Withdraw(address indexed owner, uint amount);
 
     /**
-     * @dev Emitted when user claim rewards
+     * @dev Emitted when `stakeHolder` claim rewards
      */
     event Claim(address indexed stakeHolder, uint amount);
 
     /**
-     * @dev Emitted when user claim rewards
+     * @dev Emitted when staking has started
      */
     event StartStaking(uint startPeriod, uint endingPeriod);
 }
