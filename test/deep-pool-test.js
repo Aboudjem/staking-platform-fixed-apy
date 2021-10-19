2188b5c86f8a4b927a9ba5f9aa9a20729383e91a
Aboudjem <boudjemaa.adam@gmail.com>
2021-10-19 16:58:37 +0400

    :white_check_mark: update tests

--- test/deep-pool-test.js
+++ test/deep-pool-test.js
@@ -332,7 +332,7 @@ describe("StakingPlatform - Deep Pool", () => {
   it("Should fail deposit tokens", async () => {
     await token.approve(stakingPlatform.address, 0);
     await expect(stakingPlatform.deposit(n18("100000"))).to.revertedWith(
-      "Increase allowance"
+      "ERC20: transfer amount exceeds allowance"
     );
   });
 
@@ -538,9 +538,9 @@ describe("StakingPlatform - Deep Pool", () => {
   it("Should withdraw residual balances", async () => {
     expect(
       (await token.balanceOf(stakingPlatform.address)).toString()
-    ).to.equal("4154448631250000000000000");
+    ).to.equal("4154500000000000000000000");
     expect((await token.balanceOf(addresses[0])).toString()).to.equal(
-      "992868051368750000000000000"
+      "992868000000000000000000000"
     );
 
     await token.transfer(
